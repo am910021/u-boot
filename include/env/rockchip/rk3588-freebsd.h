b1c2a94fcbeb0e55443242808b69df0c23ca363f
Yuri <am910021@gmail.com>
2026-07-30 16:56:06 +0800

    rockchip: load FreeBSD boot menu from text environment

--- include/env/rockchip/rk3588-freebsd.h
+++ include/env/rockchip/rk3588-freebsd.h
@@ -16,7 +16,47 @@
 	"bootmenu_delay=3\0" \
 	"bootmenu_0=FreeBSD=run boot_freebsd\0" \
 	"bootmenu_1=U-Boot CLI=exit\0" \
+	"bootmenu_config=/EFI/bootmenu.env\0" \
 	RK3588_FREEBSD_LOGO_SETTING \
+	"reset_bootmenu=" \
+		"setenv bootmenu_title \"*** FreeBSD U-Boot Boot Menu ***\"; " \
+		"setenv bootmenu_delay 3; " \
+		"setenv bootmenu_0 \"FreeBSD=run boot_freebsd\"; " \
+		"setenv bootmenu_1 \"U-Boot CLI=exit\"; " \
+		"setenv bootmenu_2; setenv bootmenu_3; setenv bootmenu_4; " \
+		"setenv bootmenu_5; setenv bootmenu_6; setenv bootmenu_7; " \
+		"setenv bootmenu_8; setenv bootmenu_9\0" \
+	"load_bootmenu_mmc=" \
+		"if mmc dev ${bootmenu_mmcdev}; then " \
+			"if load mmc ${bootmenu_mmcdev}:1 ${scriptaddr} " \
+					"${bootmenu_config}; then " \
+				"if env import -t -r ${scriptaddr} ${filesize} " \
+						"bootmenu_title bootmenu_delay " \
+						"bootmenu_0 bootmenu_1 bootmenu_2 " \
+						"bootmenu_3 bootmenu_4 bootmenu_5 " \
+						"bootmenu_6 bootmenu_7 bootmenu_8 " \
+						"bootmenu_9; then " \
+					"echo \"Loaded ${bootmenu_config} from mmc " \
+						"${bootmenu_mmcdev}:1\"; " \
+					"setenv bootmenu_loaded 1; " \
+				"else " \
+					"echo \"Invalid ${bootmenu_config} on mmc " \
+						"${bootmenu_mmcdev}:1\"; " \
+					"run reset_bootmenu; " \
+				"fi; " \
+			"fi; " \
+		"fi\0" \
+	"load_bootmenu=" \
+		"setenv bootmenu_loaded 0; run reset_bootmenu; " \
+		"setenv bootmenu_mmcdev 1; run load_bootmenu_mmc; " \
+		"if test \"${bootmenu_loaded}\" != \"1\"; then " \
+			"setenv bootmenu_mmcdev 0; run load_bootmenu_mmc; " \
+		"fi; " \
+		"if test \"${bootmenu_loaded}\" != \"1\"; then " \
+			"echo \"Using built-in FreeBSD boot menu\"; " \
+			"run reset_bootmenu; " \
+		"fi; " \
+		"setenv bootmenu_loaded; setenv bootmenu_mmcdev\0" \
 	"load_logo_mmc=" \
 		"if mmc dev ${logo_mmcdev}; then " \
 			"if mmc read ${loadaddr} 0x6000 0x2000; then " \
@@ -60,6 +100,7 @@
 		"setenv boot_mmcdev 0; run boot_freebsd_from_mmc; " \
 		"setenv boot_mmcdev; echo \"FreeBSD boot failed\"\0" \
 	"fallback_menu=" \
+		"run load_bootmenu; " \
 		"echo \"RK3588 FreeBSD U-Boot 2026.07\"; " \
 		"echo \"Default after timeout: FreeBSD\"; " \
 		"bootmenu ${bootmenu_delay}; run boot_freebsd; bootflow scan -lb\0" \
