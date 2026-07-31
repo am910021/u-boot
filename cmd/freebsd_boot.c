a0835e3b7c4f88fe4565b808115c33dab4a9a8a8
Yuri <am910021@gmail.com>
2026-07-31 10:58:40 +0800

    boot: apply FreeBSD default requests from ESP
    Signed-off-by: Yuri <am910021@gmail.com>

--- cmd/freebsd_boot.c
+++ cmd/freebsd_boot.c
@@ -5,14 +5,153 @@
 #include <env.h>
 #include <fs.h>
 #include <malloc.h>
+#include <mapmem.h>
 #include <mmc.h>
 #include <nvme.h>
 #include <part.h>
 #include <scsi.h>
 #include <usb.h>
+#include <linux/ctype.h>
 
 #define FREEBSD_LOADER_PATH	"/EFI/FreeBSD/loader.efi"
+#define FREEBSD_REQUEST_PATH	"/uboot-env.request"
+#define FREEBSD_REQUEST_PREFIX	"freebsd_default_boot="
 #define FREEBSD_MAX_ENTRIES	96
+
+static const char *freebsd_parse_number(const char *p, unsigned int max,
+					bool zero_ok)
+{
+	unsigned int value = 0;
+
+	if (!isdigit(*p))
+		return NULL;
+	do {
+		value = value * 10 + *p++ - '0';
+		if (value > max)
+			return NULL;
+	} while (isdigit(*p));
+
+	return value || zero_ok ? p : NULL;
+}
+
+static bool freebsd_valid_target(const char *value)
+{
+	static const char *const ifaces[] = {
+		"mmc", "usb", "nvme", "scsi",
+	};
+	const char *p;
+	int i;
+
+	if (!strcmp(value, "auto"))
+		return true;
+
+	for (i = 0; i < ARRAY_SIZE(ifaces); i++) {
+		p = value;
+		if (strncmp(p, ifaces[i], strlen(ifaces[i])))
+			continue;
+		p += strlen(ifaces[i]);
+		p = freebsd_parse_number(p, 255, true);
+		if (!p || *p++ != ':')
+			return false;
+		p = freebsd_parse_number(p, MAX_SEARCH_PARTITIONS, false);
+		if (!p)
+			return false;
+		return !*p;
+	}
+
+	return false;
+}
+
+static int freebsd_read_request(struct blk_desc *desc, int part,
+				char *value, size_t value_size)
+{
+	char request[96];
+	loff_t actread;
+	loff_t size;
+	char *end;
+
+	if (fs_set_blk_dev_with_part(desc, part) ||
+	    fs_size(FREEBSD_REQUEST_PATH, &size) ||
+	    size <= strlen(FREEBSD_REQUEST_PREFIX) ||
+	    size >= sizeof(request))
+		return -ENOENT;
+	if (fs_set_blk_dev_with_part(desc, part) ||
+	    fs_read(FREEBSD_REQUEST_PATH, map_to_sysmem(request), 0, size,
+		    &actread) ||
+	    actread != size)
+		return -EIO;
+
+	request[size] = '\0';
+	end = request + size;
+	while (end > request && (end[-1] == '\n' || end[-1] == '\r'))
+		*--end = '\0';
+
+	if (strncmp(request, FREEBSD_REQUEST_PREFIX,
+		    strlen(FREEBSD_REQUEST_PREFIX)))
+		return -EINVAL;
+	strlcpy(value, request + strlen(FREEBSD_REQUEST_PREFIX), value_size);
+
+	return freebsd_valid_target(value) ? 0 : -EINVAL;
+}
+
+static int freebsd_apply_request(struct blk_desc *desc, int part)
+{
+	const char *current;
+	char *saved = NULL;
+	char value[32];
+	int ret;
+
+	ret = freebsd_read_request(desc, part, value, sizeof(value));
+	if (ret)
+		return ret;
+
+	current = env_get("freebsd_default_boot");
+	if (current) {
+		saved = strdup(current);
+		if (!saved)
+			return -ENOMEM;
+	}
+
+	ret = env_set("freebsd_default_boot", value);
+	if (!ret)
+		ret = env_save();
+	if (ret) {
+		env_set("freebsd_default_boot", saved);
+		free(saved);
+		printf("Keeping %s on %s%d:%d: saveenv failed\n",
+		       FREEBSD_REQUEST_PATH,
+		       blk_get_uclass_name(desc->uclass_id), desc->devnum, part);
+		return ret;
+	}
+	free(saved);
+
+	if (fs_set_blk_dev_with_part(desc, part) ||
+	    fs_unlink(FREEBSD_REQUEST_PATH)) {
+		printf("Applied %s=%s; could not remove %s from %s%d:%d\n",
+		       "freebsd_default_boot", value, FREEBSD_REQUEST_PATH,
+		       blk_get_uclass_name(desc->uclass_id), desc->devnum, part);
+	} else {
+		printf("Applied %s=%s from %s%d:%d\n",
+		       "freebsd_default_boot", value,
+		       blk_get_uclass_name(desc->uclass_id), desc->devnum, part);
+	}
+
+	return 0;
+}
+
+static bool freebsd_request_on_desc(struct blk_desc *desc)
+{
+	struct disk_partition info;
+	int part;
+
+	for (part = 1; part <= MAX_SEARCH_PARTITIONS; part++) {
+		if (!part_get_info(desc, part, &info) &&
+		    !freebsd_apply_request(desc, part))
+			return true;
+	}
+
+	return false;
+}
 
 static void freebsd_clear_menu(void)
 {
@@ -94,6 +233,22 @@ static void freebsd_scan_uclass(enum uclass_id id, const char *label,
 	}
 }
 
+static bool freebsd_request_uclass(enum uclass_id id)
+{
+	struct blk_desc *desc;
+	int devnum;
+	int max;
+
+	max = blk_find_max_devnum(id);
+	for (devnum = 0; devnum <= max; devnum++) {
+		desc = blk_get_devnum_by_uclass_id(id, devnum);
+		if (desc && freebsd_request_on_desc(desc))
+			return true;
+	}
+
+	return false;
+}
+
 static void freebsd_scan_mmc(bool removable, const char *wanted, int *index,
 			     int *default_index)
 {
@@ -111,25 +266,58 @@ static void freebsd_scan_mmc(bool removable, const char *wanted, int *index,
 	}
 }
 
+static bool freebsd_request_mmc(bool removable)
+{
+	struct blk_desc *desc;
+	int devnum;
+	int max;
+
+	max = blk_find_max_devnum(UCLASS_MMC);
+	for (devnum = 0; devnum <= max; devnum++) {
+		desc = blk_get_devnum_by_uclass_id(UCLASS_MMC, devnum);
+		if (!desc || !!desc->removable != removable)
+			continue;
+		if (freebsd_request_on_desc(desc))
+			return true;
+	}
+
+	return false;
+}
+
 static int freebsd_build_menu(void)
 {
-	const char *wanted = env_get("freebsd_default_boot");
+	const char *wanted;
 	char value[16];
 	int default_index = 0;
 	int index = 0;
+	bool usb_ready;
+	bool nvme_ready;
+	bool scsi_ready;
 
 	freebsd_clear_menu();
 	mmc_initialize(NULL);
+	usb_ready = IS_ENABLED(CONFIG_USB_STORAGE) && !usb_init();
+	nvme_ready = IS_ENABLED(CONFIG_NVME) && !nvme_scan_namespace();
+	scsi_ready = IS_ENABLED(CONFIG_SCSI) && !scsi_scan(false);
+
+	if (!freebsd_request_mmc(false) &&
+	    !freebsd_request_mmc(true) &&
+	    !(usb_ready && freebsd_request_uclass(UCLASS_USB)) &&
+	    !(nvme_ready && freebsd_request_uclass(UCLASS_NVME)) &&
+	    scsi_ready)
+		freebsd_request_uclass(UCLASS_SCSI);
+
+	wanted = env_get("freebsd_default_boot");
 	freebsd_scan_mmc(false, wanted, &index, &default_index);
 	freebsd_scan_mmc(true, wanted, &index, &default_index);
 
-	if (IS_ENABLED(CONFIG_USB_STORAGE) && !usb_init())
+	if (usb_ready)
 		freebsd_scan_uclass(UCLASS_USB, "USB", wanted, &index,
 				   &default_index);
-	if (IS_ENABLED(CONFIG_NVME) && !nvme_scan_namespace())
+	if (nvme_ready)
 		freebsd_scan_uclass(UCLASS_NVME, "NVMe", wanted, &index,
 				   &default_index);
-	if (IS_ENABLED(CONFIG_SCSI) && !scsi_scan(false))
+	if (scsi_ready)
 		freebsd_scan_uclass(UCLASS_SCSI, "SATA/SCSI", wanted, &index,
 				   &default_index);
 
